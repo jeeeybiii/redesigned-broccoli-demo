2dcc9d604f0ba21d1b141f328a440ce0ab8ceaae
jeeeybiii <macuj1@student.op.ac.nz>
2026-09-18 09:48:09 +1200

    added a c# file with hello world

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace HelloWorld
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input;
+            Console.WriteLine("Hello, World!");
+            input = Console.ReadLine();
+            Console.WriteLine(input);
+
+            Console.ReadLine();
+        }
+    }
+}
